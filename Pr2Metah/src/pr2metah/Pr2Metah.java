631f136a32f86c6e2f5903f1eb3e654335f039bb
xenahort <juan.carlos.wow.95@gmail.com>
2016-10-31 11:01:05 +0100

    Añadida lectura de fichero

--- Pr2Metah/src/pr2metah/Pr2Metah.java
+++ Pr2Metah/src/pr2metah/Pr2Metah.java
@@ -5,17 +5,112 @@
  */
 package pr2metah;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author alumno
  */
 public class Pr2Metah {
 
+    static int cubre[];
+    static int matriz[][];
+    static int x, y;
+
+    public static final int SEMILLA1 = 77383426;
+    public static final int SEMILLA2 = 77368737;
+    public static final int SEMILLA3 = 34267738;
+    public static final int SEMILLA4 = 87377736;
+    public static final int SEMILLA5 = 12482498;
+    
+    /**
+     * Funcion para leer n fichero
+     * @param fich Ruta del fichero a leer
+     * @throws FicheroNoEncontrado Excepcion en caso de no encontrar el fichero
+     * @throws java.io.FileNotFoundException
+     */
+    public static void leerFichero(String fich) throws FicheroNoEncontrado, FileNotFoundException, IOException {
+        if (!(new File(fich)).exists()) {
+            throw new FicheroNoEncontrado("Fichero no encontrado \n");
+        }
+        File archivo;
+        FileReader fr = null;
+        BufferedReader br;
+        try {
+            archivo = new File(fich);
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+            String texto;
+            String[] datos;
+            texto = br.readLine();
+            datos = texto.split(" ");
+            y = Integer.parseInt(datos[1]) + 1;
+            x = Integer.parseInt(datos[2]) + 1;
+
+            matriz = new int[y][x];
+            cubre = new int[x];
+
+            for (int i = 0; i < x; i++) {
+                cubre[i] = 0;
+            }
+            for (int i = 1; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    matriz[i][j] = 0;
+                }
+            }
+            matriz[0][0] = 0;
+            int comisariasV = 1;
+            while (x != comisariasV) {
+                texto = br.readLine();
+                datos = texto.split(" ");
+                for (int i = 1; i < datos.length; i++) {
+                    matriz[0][comisariasV] = Integer.parseInt(datos[i]);
+                    ++comisariasV;
+                }
+            }
+            int cont;
+            for (int i = 1; i < y; i++) {
+                texto = br.readLine();
+                datos = texto.split(" ");
+                cont = Integer.parseInt(datos[1]);
+                while (cont != 0) {
+                    texto = br.readLine();
+                    datos = texto.split(" ");
+                    for (int j = 1; j < datos.length; j++) {
+                        matriz[i][Integer.parseInt(datos[j])] = 1;
+                        ++cubre[Integer.parseInt(datos[j])];
+                        --cont;
+                    }
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+        } finally {
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+    }
+
     /**
      * @param args the command line arguments
+     * @throws pr2metah.FicheroNoEncontrado
+     * @throws java.lang.InterruptedException
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)throws FicheroNoEncontrado, InterruptedException, IOException {
+       
+        String ficheros[] = {"scpe1.txt", "scp41.txt", "scpd1.txt", "scpnrf1.txt", "scpa1.txt"};
+        int n = 5;
+        
+        leerFichero(ficheros[0]);
+
     }
-    
+
 }
